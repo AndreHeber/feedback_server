a0d4b47e5aaffa6a0b8479274f0620da5404e290
Andre Heber <a.heber@sabienzia.com>
2023-09-05 22:50:32 +0200

    questions in database, error logging

--- main.go
+++ main.go
@@ -19,16 +19,33 @@ func init() {
 		log.Fatalf("Failed to open database: %v", err)
 	}
 
-	createTableSQL := `CREATE TABLE IF NOT EXISTS feedback (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		answer1 INTEGER,
-		answer2 INTEGER,
-		answer3 INTEGER
-	);`
-
-	_, err = db.Exec(createTableSQL)
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS questions (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        question TEXT,
+        type TEXT
+    );`)
 	if err != nil {
-		log.Fatalf("Failed to create table: %v", err)
+		log.Fatalf("Failed to create questions table: %v", err)
+	}
+
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS feedback (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        question_id INTEGER,
+        numeric_answer INTEGER,
+        text_answer TEXT,
+        FOREIGN KEY(question_id) REFERENCES questions(id)
+    );`)
+	if err != nil {
+		log.Fatalf("Failed to create feedback table: %v", err)
+	}
+
+	// Populate questions, handle errors gracefully.
+	_, err = db.Exec(`INSERT OR IGNORE INTO questions (id, question, type) VALUES 
+                     (1, 'How well do I listen to others?', 'numeric'), 
+                     (2, 'How clear is my communication?', 'numeric'), 
+                     (3, 'Any additional comments?', 'text')`)
+	if err != nil {
+		log.Printf("Failed to insert questions: %v", err)
 	}
 }
 
@@ -42,12 +59,37 @@ func main() {
 }
 
 func formPage(w http.ResponseWriter, r *http.Request) {
+	rows, err := db.Query("SELECT id, question, type FROM questions")
+	if err != nil {
+		log.Printf("Error querying questions: %v", err)
+		http.Error(w, "Failed to query database", http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var questions []map[string]interface{}
+	for rows.Next() {
+		var id int
+		var question, qType string
+		if err := rows.Scan(&id, &question, &qType); err != nil {
+			log.Printf("Error scanning rows: %v", err)
+			http.Error(w, "Failed to scan row", http.StatusInternalServerError)
+			return
+		}
+		questions = append(questions, map[string]interface{}{
+			"id":       id,
+			"question": question,
+			"type":     qType,
+		})
+	}
+
 	t, err := template.ParseFiles("form.html")
 	if err != nil {
+		log.Printf("Error parsing template: %v", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	t.Execute(w, nil)
+	t.Execute(w, questions)
 }
 
 func submitForm(w http.ResponseWriter, r *http.Request) {
@@ -56,44 +98,84 @@ func submitForm(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	answer1 := r.FormValue("answer1")
-	answer2 := r.FormValue("answer2")
-	answer3 := r.FormValue("answer3")
-
-	_, err := db.Exec("INSERT INTO feedback (answer1, answer2, answer3) VALUES (?, ?, ?)", answer1, answer2, answer3)
+	err := r.ParseForm()
 	if err != nil {
-		http.Error(w, "Failed to insert into database", http.StatusInternalServerError)
+		log.Printf("Error parsing form: %v", err)
+		http.Error(w, "Could not parse form", http.StatusBadRequest)
 		return
+	}
+
+	for key, values := range r.PostForm {
+		var questionID int
+		fmt.Sscanf(key, "q%d", &questionID)
+		if questionID == 0 {
+			continue
+		}
+
+		value := values[0]
+		row := db.QueryRow("SELECT type FROM questions WHERE id = ?", questionID)
+		var qType string
+		if err := row.Scan(&qType); err != nil {
+			log.Printf("Error scanning question type: %v", err)
+			http.Error(w, "Failed to scan row", http.StatusInternalServerError)
+			return
+		}
+
+		if qType == "numeric" {
+			_, err := db.Exec("INSERT INTO feedback (question_id, numeric_answer) VALUES (?, ?)", questionID, value)
+			if err != nil {
+				log.Printf("Error inserting numeric answer: %v", err)
+				http.Error(w, "Failed to insert into database", http.StatusInternalServerError)
+				return
+			}
+		} else if qType == "text" {
+			_, err := db.Exec("INSERT INTO feedback (question_id, text_answer) VALUES (?, ?)", questionID, value)
+			if err != nil {
+				log.Printf("Error inserting text answer: %v", err)
+				http.Error(w, "Failed to insert into database", http.StatusInternalServerError)
+				return
+			}
+		}
 	}
 
 	http.Redirect(w, r, "/results", http.StatusSeeOther)
 }
 
 func showResults(w http.ResponseWriter, r *http.Request) {
-	rows, err := db.Query("SELECT * FROM feedback")
+	rows, err := db.Query(`SELECT q.question, q.type, f.numeric_answer, f.text_answer 
+                        FROM feedback f 
+                        INNER JOIN questions q ON f.question_id = q.id`)
 	if err != nil {
+		log.Printf("Error querying results: %v", err)
 		http.Error(w, "Failed to query database", http.StatusInternalServerError)
 		return
 	}
 	defer rows.Close()
 
-	var results []map[string]int
+	var results []map[string]interface{}
 	for rows.Next() {
-		var id, answer1, answer2, answer3 int
-		if err := rows.Scan(&id, &answer1, &answer2, &answer3); err != nil {
+		var question, qType string
+		var numericAnswer sql.NullInt64
+		var textAnswer sql.NullString
+		if err := rows.Scan(&question, &qType, &numericAnswer, &textAnswer); err != nil {
+			log.Printf("Error scanning results: %v", err)
 			http.Error(w, "Failed to scan row", http.StatusInternalServerError)
 			return
 		}
-		results = append(results, map[string]int{
-			"answer1": answer1,
-			"answer2": answer2,
-			"answer3": answer3,
+		results = append(results, map[string]interface{}{
+			"question":      question,
+			"type":          qType,
+			"numericAnswer": numericAnswer,
+			"textAnswer":    textAnswer,
 		})
 	}
 
 	t, err := template.ParseFiles("results.html")
-	if err != nil {
-		http.Error(w, "Failed to load template", http.StatusInternalServerError)
+	if err !=
+
+ nil {
+		log.Printf("Error parsing template: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	t.Execute(w, results)
